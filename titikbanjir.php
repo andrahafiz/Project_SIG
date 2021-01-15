9551c3122fa550b7d299fd53c1c4f53f229d4ee8
andrahafiz <andra18ti@mahasiswa.pcr.ac.id>
2021-01-15 20:00:08 +0700

    Update

--- titikbanjir.php
+++ titikbanjir.php
@@ -5,35 +5,22 @@ include 'header.php';
 <!-- Achievement Section Begin -->
 <section class="achievement-section set-bg spad" data-setbg="img/achievement-bg.jpg">
   <div class="container mt-5">
-    <div class="row">
+    <div class="row justify-content-md-center">
       <div class="col-lg-3 col-md-3 col-sm-6">
         <div class="achievement__item">
           <span class="fa fa-user-o"></span>
-          <h2 class="achieve-counter">2468</h2>
-          <p>Clients</p>
+          <h2 class="achieve-counter">6</h2>
+          <p>Titik Banjir</p>
         </div>
       </div>
       <div class="col-lg-3 col-md-3 col-sm-6">
         <div class="achievement__item">
           <span class="fa fa-edit"></span>
-          <h2 class="achieve-counter">2468</h2>
-          <p>Domains</p>
+          <h2 class="achieve-counter">4</h2>
+          <p>Parit Tersumbat</p>
         </div>
       </div>
-      <div class="col-lg-3 col-md-3 col-sm-6">
-        <div class="achievement__item">
-          <span class="fa fa-clone"></span>
-          <h2 class="achieve-counter">2468</h2>
-          <p>Server</p>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-3 col-sm-6">
-        <div class="achievement__item">
-          <span class="fa fa-cog"></span>
-          <h2 class="achieve-counter">2468</h2>
-          <p>Installs</p>
-        </div>
-      </div>
+
     </div>
   </div>
 </section>
@@ -45,7 +32,7 @@ include 'header.php';
     <div class="row">
       <div class="col-lg-12">
         <div class="section-title">
-          <h3>HOW TO BUILD YOUR WEBSITE ONLINE TODAY?</h3>
+          <h3>Titik Banjir</h3>
         </div>
         <div class="work__text">
           <div class="row">
