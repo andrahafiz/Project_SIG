b476eb339eea2b88ebcc7c4a225afe0393752866
andrahafiz <andra18ti@mahasiswa.pcr.ac.id>
2021-01-16 23:34:54 +0700

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -91,27 +91,6 @@ var vectorLayer = new ol.layer.Vector({
   map_titikbanjir.addOverlay(overlay);
 
   map_titikbanjir.on('click', function(evt) {
-    var feature = map.forEachFeatureAtPixel(evt.pixel,
-      function(feature, layer) {
-        return feature;
-      });
-
-    if (feature) {
-      var geometry = feature.getGeometry();
-      var coord = geometry.getCoordinates();
-
-      var content = '<h3>Nama Tempat : ' + feature.get('Lebar') + '</h3>';
-      // content += '<h3>Jumlah Korban : ' + feature.get('Jumlah_Korban') + '</h3>';
-      // content += '<img src="https://drive.google.com/file/d/1EaZgWu1YzOmx6GqODlj2XUn_NeZaOLs8" width="300"/>';
-
-      content_element.innerHTML = content;
-      overlay.setPosition(coord);
-      console.info(feature.getProperties());
-
-    }
-  });
-
-  map_titikbanjir.on('click', function(evt) {
     var feature = map_titikbanjir.forEachFeatureAtPixel(evt.pixel,
       function(feature, layer) {
         return feature;
