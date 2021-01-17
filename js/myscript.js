8ec9d0cbe1f779bb44cc2caaf386c0a61aa436ad
andrahafiz <andra18ti@mahasiswa.pcr.ac.id>
2021-01-17 13:46:38 +0700

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,167 +1,217 @@
 var vectorLayer = new ol.layer.Vector({
-    source: new ol.source.Vector({
-      format: new ol.format.GeoJSON(),
-      url: 'json/banjir.json'
+  source: new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: 'json/banjir.json'
+  }),
+  style: new ol.style.Style({
+    image: new ol.style.Icon(({
+      anchor: [0.5, 46],
+      anchorXUnits: 'fraticon',
+      anchorYUnits: 'pixels',
+      src: 'icon/pin.png'
+    }))
+  })
+});
+
+var vectorLayerDrainase = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: 'json/GeoJSON_Line.json'
+  }),
+  style: new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: '#2753e5',
+      width: 4
+    })
+  })
+});
+
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
     }),
-    style: new ol.style.Style({
-      image: new ol.style.Icon(({
-        anchor: [0.5, 46],
-        anchorXUnits: 'fraticon',
-        anchorYUnits: 'pixels',
-        src: 'icon/pin.png'
-      }))
-    })
-  });
+    vectorLayerDrainase
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([101.436, 0.545]),
+    zoom: 16
+  })
+});
 
-  var vectorLayerDrainase = new ol.layer.Vector({
-    source: new ol.source.Vector({
-      format: new ol.format.GeoJSON(),
-      url: 'json/GeoJSON_Line.json'
+var vectorLayer_titikbanjir = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: 'json/TitikBanjir.json'
+  }),
+  style: new ol.style.Style({
+    image: new ol.style.Icon(({
+      anchor: [0.5, 46],
+      anchorXUnits: 'fraticon',
+      anchorYUnits: 'pixels',
+      src: 'icon/pin.png'
+    }))
+  })
+});
+
+var container = document.getElementById('popup'),
+  content_element = document.getElementById('popup-content'),
+  closer = document.getElementById('popup-closer');
+
+closer.onclick = function () {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+
+};
+var overlay = new ol.Overlay({
+  element: container,
+  autoPan: true,
+  offset: [0, -10]
+});
+
+map.addOverlay(overlay);
+
+var fullscreen = new ol.control.FullScreen();
+map.addControl(fullscreen);
+
+// CREATE MAP TITIK BANJIR
+var map_titikbanjir = new ol.Map({
+  target: 'map_titikbanjir',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
     }),
-    style:new ol.style.Style({
-      stroke: new ol.style.Stroke({color: '#2753e5',width:4})
-    })
-  });
+    vectorLayer_titikbanjir
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([101.436, 0.545]),
+    zoom: 16
+  })
+});
 
-  var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      }),
-      vectorLayerDrainase
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([101.436, 0.545]),
-      zoom: 16
-    })
-  });
+map_titikbanjir.addOverlay(overlay);
 
-  var vectorLayer_titikbanjir = new ol.layer.Vector({
-    source: new ol.source.Vector({
-      format: new ol.format.GeoJSON(),
-      url: 'json/TitikBanjir.json'
+map_titikbanjir.on('click', function (evt) {
+  var feature = map_titikbanjir.forEachFeatureAtPixel(evt.pixel,
+    function (feature, layer) {
+      return feature;
+    });
+
+  if (feature) {
+    var geometry = feature.getGeometry();
+    var coord = geometry.getCoordinates();
+
+    var content = '<p class="text-center font-weight-bold m-0">Nama Jalan</p><p class="text-center m-0">' + feature.get('Nama_Jalan') + '</p>';
+    // var content = '<p class="text-center font-weight-bold m-0">Nama Jalan</p><p class="text-center m-0">' + feature.get('Nama_Jalan') + '</p>';
+    // content += '<h3>Jumlah Korban : ' + feature.get('Jumlah_Korban') + '</h3>';
+    // content += '<img src="https://drive.google.com/file/d/1EaZgWu1YzOmx6GqODlj2XUn_NeZaOLs8" width="300"/>';
+
+    content_element.innerHTML = content;
+    overlay.setPosition(coord);
+    console.info(feature.getProperties());
+
+  }
+});
+
+// CREATE MAP TITIK TERSUMBAT
+var vectorLayer_titiktersumbat = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: 'json/TitikTersumbat.json'
+  }),
+  style: new ol.style.Style({
+    image: new ol.style.Icon(({
+      anchor: [0.5, 46],
+      anchorXUnits: 'fraticon',
+      anchorYUnits: 'pixels',
+      src: 'icon/pin.png'
+    }))
+  })
+});
+var map_titiktersumbat = new ol.Map({
+  target: 'map_titiktersumbat',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
     }),
-    style: new ol.style.Style({
-      image: new ol.style.Icon(({
-        anchor: [0.5, 46],
-        anchorXUnits: 'fraticon',
-        anchorYUnits: 'pixels',
-        src: 'icon/pin.png'
-      }))
-    })
-  });
+    vectorLayer_titiktersumbat
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([101.449, 0.545]),
+    zoom: 16
+  })
+});
 
-  var container = document.getElementById('popup'),
-    content_element = document.getElementById('popup-content'),
-    closer = document.getElementById('popup-closer');
+map_titiktersumbat.addOverlay(overlay);
 
-  closer.onclick = function() {
-    overlay.setPosition(undefined);
-    closer.blur();
-    return false;
+// map_titiktersumbat.on('click', function (evt) {
+//   var feature = map_titiktersumbat.forEachFeatureAtPixel(evt.pixel,
+//     function (feature, layer) {
+//       return feature;
+//     });
 
-  };
-  var overlay = new ol.Overlay({
-    element: container,
-    autoPan: true,
-    offset: [0, -10]
-  });
+//   if (feature) {
+//     var geometry = feature.getGeometry();
+//     var coord = geometry.getCoordinates();
+//     // console.log("Hi apa kabar!");
 
-  map.addOverlay(overlay);
+//     var content = '<p class="text-center font-weight-bold m-0">Nama Jalana</p><p class="text-center m-0">' + feature.get('Nama Jalan') + '</p>';
+//     // var content = '<p class="text-center font-weight-bold m-0">Nama Jalan</p><p class="text-center m-0">' + feature.get('Nama_Jalan') + '</p>';
+//     // content += '<h3>Jumlah Korban : ' + feature.get('Jumlah_Korban') + '</h3>';
+//     // content += '<img src="https://drive.google.com/file/d/1EaZgWu1YzOmx6GqODlj2XUn_NeZaOLs8" width="300"/>';
 
-  var fullscreen = new ol.control.FullScreen();
-  map.addControl(fullscreen);
+//     content_element.innerHTML = content;
+//     overlay.setPosition(coord);
+//     console.info(feature.getProperties());
 
-  // CREATE MAP TITIK BANJIR
-  var map_titikbanjir = new ol.Map({
-    target: 'map_titikbanjir',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      }),
-      vectorLayer_titikbanjir
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([101.436, 0.545]),
-      zoom: 16
-    })
-  });
-  
-  map_titikbanjir.addOverlay(overlay);
+//   }
+// });
 
-  map_titikbanjir.on('click', function(evt) {
-    var feature = map_titikbanjir.forEachFeatureAtPixel(evt.pixel,
-      function(feature, layer) {
-        return feature;
-      });
+map_titiktersumbat.addEventListener("mouseover", function () {
+  var feature = map_titiktersumbat.forEachFeatureAtPixel(evt.pixel,
+    function (feature, layer) {
+      return feature;
+    });
 
-    if (feature) {
-      var geometry = feature.getGeometry();
-      var coord = geometry.getCoordinates();
+  if (feature) {
+    var geometry = feature.getGeometry();
+    var coord = geometry.getCoordinates();
+    console.log("Hi apa kabar!");
 
-      var content = '<p class="text-center font-weight-bold m-0">Nama Jalan</p><p class="text-center m-0">' + feature.get('Nama_Jalan') + '</p>';
-      // var content = '<p class="text-center font-weight-bold m-0">Nama Jalan</p><p class="text-center m-0">' + feature.get('Nama_Jalan') + '</p>';
-      // content += '<h3>Jumlah Korban : ' + feature.get('Jumlah_Korban') + '</h3>';
-      // content += '<img src="https://drive.google.com/file/d/1EaZgWu1YzOmx6GqODlj2XUn_NeZaOLs8" width="300"/>';
+    var content = '<p class="text-center font-weight-bold m-0">Nama Jalana</p><p class="text-center m-0">' + feature.get('Nama Jalan') + '</p>';
+    // var content = '<p class="text-center font-weight-bold m-0">Nama Jalan</p><p class="text-center m-0">' + feature.get('Nama_Jalan') + '</p>';
+    // content += '<h3>Jumlah Korban : ' + feature.get('Jumlah_Korban') + '</h3>';
+    // content += '<img src="https://drive.google.com/file/d/1EaZgWu1YzOmx6GqODlj2XUn_NeZaOLs8" width="300"/>';
 
-      content_element.innerHTML = content;
-      overlay.setPosition(coord);
-      console.info(feature.getProperties());
+    content_element.innerHTML = content;
+    overlay.setPosition(coord);
+    console.info(feature.getProperties());
 
-    }
-  });
+  }
+});
+// console.log(map_titiktersumbat)
+// map_titiktersumbat.on("mouseleave", function () {
+//   // masukin scriptnya disini.
+//   // var feature = map_titiktersumbat.forEachFeatureAtPixel(evt.pixel,
+//   //   function (feature, layer) {
+//   //     return feature;
+//   //   });
 
-  // CREATE MAP TITIK TERSUMBAT
-  var vectorLayer_titiktersumbat = new ol.layer.Vector({
-    source: new ol.source.Vector({
-      format: new ol.format.GeoJSON(),
-      url: 'json/TitikTersumbat.json'
-    }),
-    style: new ol.style.Style({
-      image: new ol.style.Icon(({
-        anchor: [0.5, 46],
-        anchorXUnits: 'fraticon',
-        anchorYUnits: 'pixels',
-        src: 'icon/pin.png'
-      }))
-    })
-  });
-  var map_titiktersumbat = new ol.Map({
-    target: 'map_titiktersumbat',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      }),
-      vectorLayer_titiktersumbat
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([101.449, 0.545]),
-      zoom: 16
-    })
-  });
-  
-  map_titiktersumbat.addOverlay(overlay);
+//   // if (feature) {
+//   //   var geometry = feature.getGeometry();
+//   //   var coord = geometry.getCoordinates();
+//   //   // console.log("Hi apa kabar!");
 
-  map_titiktersumbat.on('click', function(evt) {
-    var feature = map_titiktersumbat.forEachFeatureAtPixel(evt.pixel,
-      function(feature, layer) {
-        return feature;
-      });
+//   //   var content = '<p class="text-center font-weight-bold m-0">Nama Jalana</p><p class="text-center m-0">' + feature.get('Nama Jalan') + '</p>';
+//   //   // var content = '<p class="text-center font-weight-bold m-0">Nama Jalan</p><p class="text-center m-0">' + feature.get('Nama_Jalan') + '</p>';
+//   //   // content += '<h3>Jumlah Korban : ' + feature.get('Jumlah_Korban') + '</h3>';
+//   //   // content += '<img src="https://drive.google.com/file/d/1EaZgWu1YzOmx6GqODlj2XUn_NeZaOLs8" width="300"/>';
 
-    if (feature) {
-      var geometry = feature.getGeometry();
-      var coord = geometry.getCoordinates();
-      // console.log("Hi apa kabar!");
+//   //   content_element.innerHTML = content;
+//   //   overlay.setPosition(coord);
+//   //   console.info(feature.getProperties());
 
-      var content = '<p class="text-center font-weight-bold m-0">Nama Jalana</p><p class="text-center m-0">' + feature.get('Nama Jalan') + '</p>';
-      // var content = '<p class="text-center font-weight-bold m-0">Nama Jalan</p><p class="text-center m-0">' + feature.get('Nama_Jalan') + '</p>';
-      // content += '<h3>Jumlah Korban : ' + feature.get('Jumlah_Korban') + '</h3>';
-      // content += '<img src="https://drive.google.com/file/d/1EaZgWu1YzOmx6GqODlj2XUn_NeZaOLs8" width="300"/>';
-
-      content_element.innerHTML = content;
-      overlay.setPosition(coord);
-      console.info(feature.getProperties());
-
-    }
-  });
+//   // }
+// });
