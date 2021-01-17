cf546b8b05aa4ce1504684423338e553bc487742
andrahafiz <andra18ti@mahasiswa.pcr.ac.id>
2021-01-18 00:05:27 +0700

    Update drainase.php

--- drainase.php
+++ drainase.php
@@ -87,8 +87,44 @@ include 'header.php';
   </div>
 </section>
 
+
+
+<!-- Services Section Begin -->
+<section class="services-section spad">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="section-title">
+          <h3>Peta Drainase</h3>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg col-md col-sm">
+        <div class="services__item">
+          <h5>Pekanbaru</h5>
+          <span>Kec. Umban Sari , Kel. Meranti Pandak</span>
+
+          <div id="map" class="map">
+            <!-- ISI MAP -->
+          </div>
+
+
+          <div id="popup" class="ol-popup">
+            <!-- variabel container-->
+            <a href="#" id="popup-closer" class="ol-popup-closer"></a> <!-- variabel closer-->
+            <div id="popup-content"></div> <!-- variabel content_element-->
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+<!-- Services Section End -->
+
 <!-- About Section Begin -->
-<section class="register-domain spad">
+<section class="register-domain spad visible">
   <div class="container">
     <div class="row d-flex justify-content-center">
       <div class="register__text text-center">
@@ -96,7 +132,7 @@ include 'header.php';
           <h3>Filter Data</h3>
         </div>
         <div class="m-5">
-          <a href="#" class="primary-btn"> Bahan Drainase </a>
+          <a href="#map" class="primary-btn"> Bahan Drainase </a>
           <a href="#" class="primary-btn"> Jalan </a>
           <a href="#" class="primary-btn"> Kondisi Drainase </a>
         </div>
@@ -139,37 +175,4 @@ include 'header.php';
   </div>
 </section>
 <!-- About Section End -->
-
-<!-- Services Section Begin -->
-<section class="services-section spad">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="section-title">
-          <h3>Peta Drainase</h3>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg col-md col-sm">
-        <div class="services__item">
-          <h5>Pekanbaru</h5>
-          <span>Kec. Umban Sari , Kel. Meranti Pandak</span>
-
-          <div id="map" class="map">
-            <!-- ISI MAP -->
-          </div>
-
-          <div id="popup" class="ol-popup">
-            <!-- variabel container-->
-            <a href="#" id="popup-closer" class="ol-popup-closer"></a> <!-- variabel closer-->
-            <div id="popup-content"></div> <!-- variabel content_element-->
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </div>
-</section>
-<!-- Services Section End -->
 <?php include 'footer.php'; ?>
