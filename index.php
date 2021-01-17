d389bc5f931a20006ee6e815cec324cf9dced43e
andrahafiz <andra18ti@mahasiswa.pcr.ac.id>
2021-01-17 13:22:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -54,65 +54,65 @@ include 'header.php';
           <div class="section-title">
             <h3>Project Drainase</h3>
           </div>
-          <p>Project ini merupakan project mata kuliah Bengkel Sistem Informasi Geografis. 
-            Project ini berisi tentang Informasi Drainase-drainase yang berada didaerah Rumbai, 
+          <p>Project ini merupakan project mata kuliah Bengkel Sistem Informasi Geografis.
+            Project ini berisi tentang Informasi Drainase-drainase yang berada didaerah Rumbai,
             tepatnya di Kecamatan Merpati Pandak </p>
         </div>
 
-        <br/><br/>
+        <br /><br />
 
         <div class="section-title">
-            <h3>DRAINASE</h3>
-          </div>
-         
-          <p>Drainase adalah lengkungan atau saluran air di permukaan atau di bawah
-            tanah, baik yang terbentuk secara alami maupun dibuat manusia. Dalam Bahasa
-            Indonesia, drainase bisa merujuk pada parit di permukaan tanah atau gorong –
-            gorong dibawah tanah.</p>
-        </div>    
+          <h3>DRAINASE</h3>
+        </div>
+
+        <p>Drainase adalah lengkungan atau saluran air di permukaan atau di bawah
+          tanah, baik yang terbentuk secara alami maupun dibuat manusia. Dalam Bahasa
+          Indonesia, drainase bisa merujuk pada parit di permukaan tanah atau gorong –
+          gorong dibawah tanah.</p>
+      </div>
 
 
       <div class="row d-flex justify-content-center">
-      <div class="col-lg-8">
-      <div class="register__text"><br/><br/>
-          <div class="section-title">
-            <h3>Arus Hilir</h3>
+        <div class="col-lg-8">
+          <div class="register__text"><br /><br />
+            <div class="section-title">
+              <h3>Arus Hilir</h3>
+            </div>
+
+            <p>Hilir atau muara sungai adalah ujung akhir aliran sungai. Adapun ciri-ciri dari arus Hilir yaitu :
+              Pengikisan atau erosi melebar ke dinding sungai, Banyak pengendapan,
+              Muara terkadang membentuk delta atau tanah datar hasil pengendapan dan sungai mati (oxbow lake) </p>
           </div>
-         
-          <p>Hilir atau muara sungai adalah ujung akhir aliran sungai. Adapun ciri-ciri dari arus Hilir yaitu :
-            Pengikisan atau erosi melebar ke dinding sungai, Banyak pengendapan,
-            Muara terkadang membentuk delta atau tanah datar hasil pengendapan dan sungai mati (oxbow lake) </p>
-        </div>
         </div>
 
         <div class="row d-flex justify-content-center">
-        <div class="col-lg-8">
-        <div class="register__text"><br/><br/>
-        <div class="section-title">
-              <h3>Arus Hulu</h3>
+          <div class="col-lg-8">
+            <div class="register__text"><br /><br />
+              <div class="section-title">
+                <h3>Arus Hulu</h3>
+              </div>
+
+              <p>Hulu adalah awal mula aliran sungai, merupakan bagian sungai yang terletak di daerah pegunungan atau perbukitan.
+                Adapun ciri-ciri dari arus Hulu yaitu : Arus deras, Pengikisan atau erosi ke dasar sungai, Saluran berbentuk VI,
+                Tidak terdapat pengendapan, Batu masih besar-besar,
+                Terdapat jeram atau air terjun </p>
             </div>
-          
-            <p>Hulu adalah awal mula aliran sungai, merupakan bagian sungai yang terletak di daerah pegunungan atau perbukitan. 
-            Adapun ciri-ciri dari arus Hulu yaitu : Arus deras, Pengikisan atau erosi ke dasar sungai, Saluran berbentuk VI, 
-            Tidak terdapat pengendapan, Batu masih besar-besar,
-            Terdapat jeram atau air terjun </p>
-          </div>
           </div>
 
-        <div class="row d-flex justify-content-center">
-        <div class="col-lg-8">
-        <div class="register__text"><br/><br/>
-        <div class="section-title">
-              <h3>Saluran paling bagus</h3>
+          <div class="row d-flex justify-content-center">
+            <div class="col-lg-8">
+              <div class="register__text"><br /><br />
+                <div class="section-title">
+                  <h3>Saluran paling bagus</h3>
+                </div>
+                asdasdsa
+                <p>Menurut peneliti serta standar penilaian drainase yang bagus .... wqweqwe
+                  qweqweqweqw qewqweqweqwe qweweqweqweewasdsadadasdsadas</p>
+              </div>
             </div>
-          
-            <p>Menurut peneliti serta standar penilaian drainase yang bagus .... wqweqwe
-            qweqweqweqw qewqweqweqwe qweweqweqweewasdsadadasdsadas</p>
-          </div>
-          </div>
 
-    </div>
-  </div>
+          </div>
+        </div>
 </section>
 
 
