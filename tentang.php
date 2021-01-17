99042cc3345fe677887167021290d540fbd997e6
AhmadNuzulAzmi <52515316+AhmadNuzulAzmi@users.noreply.github.com>
2021-01-18 04:41:11 +0700

    e

--- tentang.php
+++ tentang.php
@@ -35,10 +35,98 @@ include 'header.php';
 </section>
 <!-- Choose Plan Section Begin -->
 <section class="choose-plan-section spad">
-    <div class="container">
+    <div class="container m-5">
     </div>
 </section>
 <!-- Choose Plan Section End -->
+<br/><br/><br/>
+<!-- Work Section Begin -->
+<section class="work-section spad border-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-title">
+                    <h3 style="color:#000000;">Kontak</h3>
+                </div>
+                <!-- Team Section Begin -->
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6">
+                                <div class="team__item">
+                                    <div class="team__pic">
+                                        <img src="img/team/abdul.png" alt="">
+                                    </div>
+                                    <div class="team__text">
+                                        <h5>Abdul Arif Pratama</h5>
+                                        <span>Team Member </span>
+                                        <p>1855301051</p>
+                                        <div class="team__social">
+                                            <a href="https://web.whatsapp.com/" class="whatsapp"><i
+                                                    class="fa fa-whatsapp"> 085965423100 </i></a>
+                                            <a href="https://www.instagram.com/abdularifpratama/" class="instagram"><i
+                                                    class="fa fa-instagram"> abdularifpratama</i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6">
+                                <div class="team__item">
+                                    <div class="team__pic">
+                                        <img src="img/team/azmi.png" alt="">
+                                    </div>
+                                    <div class="team__text">
+                                        <h5>Ahmad Nuzul Azmi</h5>
+                                        <span>Team Member</span>
+                                        <p>1855301017</p>
+                                        <div class="team__social">
+                                            <a href="https://web.whatsapp.com/" class="whatsapp"><i
+                                                    class="fa fa-whatsapp"> 082275341300</i></a>
+                                            <a href="https://www.instagram.com/ahmadnuzull13/" class="instagram"><i
+                                                    class="fa fa-instagram"> ahmadnuzull13</i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6">
+                                <div class="team__item">
+                                    <div class="team__pic">
+                                        <img src="img/team/andra.png" alt="">
+                                    </div>
+                                    <div class="team__text">
+                                        <h5>Andra Al-Hafiz Hasibuan</h5>
+                                        <span>Team Member</span>
+                                        <p>1855301038</p>
+                                        <div class="team__social">
+                                            <a href="https://web.whatsapp.com/" class="whatsapp"><i
+                                                    class="fa fa-whatsapp"> 085276451200 </i></a>
+                                            <a href="https://www.instagram.com/andra_hafiz_hsb/" class="instagram"><i
+                                                    class="fa fa-instagram"> andra_hafiz_hsb</i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6">
+                                <div class="team__item">
+                                    <div class="team__pic">
+                                        <img src="img/team/fikri.png" alt="">
+                                    </div>
+                                    <div class="team__text">
+                                        <h5>M. Fikri Ramadhan</h5>
+                                        <span>Team Member</span>
+                                        <p>1855301045</p>
+                                        <div class="team__social">
+                                            <a href="https://web.whatsapp.com/" class="whatsapp"><i
+                                                    class="fa fa-whatsapp"> 085214758321 </i></a>
+                                            <a href="https://www.instagram.com/mufirdhan/" class="instagram"><i class="fa fa-instagram"> mufirdhan</i></a>
+                                        </div>
+                                    </div>
+                <!-- Team Section End -->
+
+            </div>
+        </div>
+    </div>
+</section>
+<br/><br/>
+<!-- Work Section End -->
 
 <?php
 include 'footer.php';
